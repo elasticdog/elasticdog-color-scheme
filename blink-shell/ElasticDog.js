c2268253e9642dbef1fbb352229198d5ee35e802
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-03-28 10:16:24 -0700

    Add theme for the Blink Shell mobile terminal
    See:
    - https://blink.sh/
    - https://github.com/blinksh/themes

---
+++ blink-shell/ElasticDog.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#1b2229", "#e45649", "#50a14f", "#c5a332", "#275fe4", "#a626a4", "#005478", "#c6c7c7", "#a0a1a7", "#dd8844", "#44b9b1", "#c0d9f2", "#0098dd", "#823ff1", "#0078ab", "#fafbfc"]);
+t.prefs_.set('foreground-color', "#393f46");
+t.prefs_.set('background-color', "#edeeef");
+t.prefs_.set('cursor-color', '#0098dd');
